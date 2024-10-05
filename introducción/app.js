91289464eda5979a8784652d21e80e18fa9ece6f
gabriela <bedoyagabriela28@gmail.com>
2024-10-05 10:41:24 -0500

    FirstCommit

---
+++ introducción/app.js
@@ -0,0 +1,9 @@
+// capturar un botón cuyo id es: main_button y anclar un evento click a ese botón
+let button = document.getElementById("main_button");
+button.addEventListener("click", function(){
+    alert("hola mundo");
+
+});
+
+// let main_div =document.querySelector('.main_div');
+
